c33735ff1ca16afdd057a339d4abecb8aefceb24
cezetos <grzegorz.grabowski11@gmail.com>
2016-06-02 06:42:39 +0200

    first

---
+++ MobileStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory
+    {
+    }
+}
